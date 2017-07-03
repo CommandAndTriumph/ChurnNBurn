ce59c4ffde7a4a10cb0a9f838f2737c8eeb73d55
Alan Campbell <ahcampbellster@gmail.com>
2017-07-03 12:13:04 -0700

    Added FlaskJSON support, offer types.

--- ChurnApp.py
+++ ChurnApp.py
@@ -1,9 +1,34 @@
+from flask_json import FlaskJSON, JsonError, json_response, as_json
 from flask import Flask
+import os
 
 app = Flask(__name__)
+FlaskJSON(app)
 
-@ app.route('/status')
+@app.route('/status')
 def status():
-    return 'Server is online... Faggot'
+    return json_response(status = 'Online')
 
-app.run()
+@app.route('/offers')
+@app.route('/offers/<offer_type>')
+def offers(offer_type = None):
+    if str.lower(offer_type) == 'credit':
+        churntype = 'CC_'
+    elif str.lower(offer_type) == 'savings':
+        churntype = 'SA_'
+    elif str.lower(offer_type) == 'checking':
+        churntype = 'CA_'
+    else:
+        raise ValueError('You have not selected a valid offer type.')
+    return json_response(offer_type = offer_type, offers = [x for x in os.listdir('JSON') if x.startswith(churntype)])
+
+@app.route('/offers/<offer_type>/<offer_name>')
+def offer_names(offer_name = None):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
