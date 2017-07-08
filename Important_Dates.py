ba59dbf8ca780d5f19aa56ce721d3a00f18eecc9
Alan Campbell <ahcampbellster@gmail.com>
2017-07-08 13:16:14 -0700

    Modified Important_Dates.py to exit if an invalid date is given as an input, and return a message stating why the date is invalid.

--- Important_Dates.py
+++ Important_Dates.py
@@ -2,29 +2,36 @@ import sys, datetime, re
 from Churn_Models import*
 from dateutil.relativedelta import relativedelta
 offer_name = sys.argv[1]
-application_date = datetime.datetime.strptime(sys.argv[2], '%Y-%m-%d')
+try:
+    application_date = datetime.datetime.strptime(sys.argv[2], '%Y-%m-%d')
+except ValueError:
+    print("Please enter a valid date time (YYYY-MM-DD)")
+    sys.exit(1)
 jsonread = Offer.read_from_json_file(f'JSON/{offer_name}.json')
 offer_name_simp = re.sub('(CC_|SA_|CA_)', '', offer_name)
-if offer_name.startswith('CC_') == True:
+if offer_name.startswith('CC_'):
     offer_type = 'credit card'
     offer_type_caps = 'Credit card'
-elif offer_name.startswith('SA_') == True:
+elif offer_name.startswith('SA_'):
     offer_type = 'savings account'
     offer_type_caps = 'Savings account'
-elif offer_name.startswith('CA_') ==  True:
+elif offer_name.startswith('CA_'):
     offer_type = 'checking account'
     offer_type_caps = 'Checking account'
 formatted_issue_date = jsonread.issue_date.strftime('%Y-%m-%d')
 formatted_application_date = application_date.strftime('%Y-%m-%d')
+if jsonread.issue_date > application_date:
+    print(f'This offer has not been issued yet, it will be issued on {jsonread.issue_date}')
+    sys.exit(1)
 print(f'{formatted_issue_date} * Offer for {offer_name_simp} {offer_type} issued')
 print('           |')
 print('           |')
-if jsonread.issue_date > application_date:
-    print('This offer has not been issued yet')
-elif jsonread.issue_date + jsonread.unacted_expiration_timedelta + jsonread.action_timedelta >= application_date:
+
+if jsonread.issue_date + jsonread.unacted_expiration_timedelta + jsonread.action_timedelta >= application_date:
     print(f'{formatted_application_date} * Application submitted (hypothetical)')
 else:
-    print('Offer has already expired.')
+    print(f'Offer has already expired, it expired on {jsonread.issue_date + jsonread.unacted_expiration_timedelta}')
+    sys.exit(1)
 
 offer_to_completion_timedelta = relativedelta(months = jsonread.account_active_age)
 offer_post_completion_timedelta = relativedelta(months = jsonread.account_dormant_age)
